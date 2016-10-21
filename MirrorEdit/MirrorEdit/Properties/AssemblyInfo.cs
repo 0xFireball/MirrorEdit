33332378ef3516ca52477deb663e8f4ea6434cb2
0xFireball <0xFireball@outlook.com>
2016-10-20 17:55:24 -0700

    update metadata

--- MirrorEdit/MirrorEdit/Properties/AssemblyInfo.cs
+++ MirrorEdit/MirrorEdit/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MirrorEdit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("IridiumIon MirrorEdit")]
+[assembly: AssemblyDescription("A code editor control for AvaloniaUI")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MirrorEdit")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("IridiumIon Software")]
+[assembly: AssemblyProduct("IridiumIon MirrorEdit")]
+[assembly: AssemblyCopyright("Copyright ©  0xFireball, IridiumIon Software 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
